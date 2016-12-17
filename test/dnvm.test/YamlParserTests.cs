ba14fc1bdf8ed680ee6fa8607de5535b41d9e1be
Nate McMaster <nate.mcmaster@gmail.com>
2016-12-17 10:20:39 -0800

    Scaffold test project

---
+++ test/dnvm.test/YamlParserTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DotNet.Config
+{
+    public class YamlParserTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
