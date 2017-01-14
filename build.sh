198e17208dd9d6b647eee342e18baebe6cef7ef9
Nate McMaster <nate.mcmaster@gmail.com>
2017-01-14 15:53:35 -0800

    Create build log

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-dotnet_version=1.0.0-preview4-004233
+dotnet_version=$(<./toolsversion.txt)
 DOTNET_HOME=/usr/local/share/dnvm/environments/default
 if [[ ! -d "$DOTNET_HOME/sdk/$dotnet_version" ]] ; then
     ../scripts/dotnet-install.sh --install-dir $DOTNET_HOME --version $dotnet_version
@@ -12,7 +12,9 @@ if [[ ! -d "$DOTNET_HOME/shared/Microsoft.NETCore.App/1.1.0" ]] ; then
     ../scripts/dotnet-install.sh --install-dir $DOTNET_HOME --version 1.1.0 --channel release/1.1.0 --shared-runtime
 fi
 
+mkdir -p artifacts/log/
+
 $DOTNET_HOME/dotnet restore /nologo dnvm.sln
-$DOTNET_HOME/dotnet publish /nologo /m build.proj
+$DOTNET_HOME/dotnet publish build.proj /nologo /m /fl /flp:LogFile=artifacts/log/msbuild.log
 
 echo 'Build succeeded'
