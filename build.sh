ed4fed0feae44c306d8a0f87ae79d900eedc63c6
Nate McMaster <nate.mcmaster@gmail.com>
2017-01-14 15:07:44 -0800

    Create build scripts

--- build.sh
+++ build.sh
@@ -3,28 +3,16 @@
 set -e
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+dotnet_version=1.0.0-preview4-004233
+DOTNET_HOME=/usr/local/share/dnvm/environments/default
+if [[ ! -d "$DOTNET_HOME/sdk/$dotnet_version" ]] ; then
+    ../scripts/dotnet-install.sh --install-dir $DOTNET_HOME --version $dotnet_version
+fi
+if [[ ! -d "$DOTNET_HOME/shared/Microsoft.NETCore.App/1.1.0" ]] ; then
+    ../scripts/dotnet-install.sh --install-dir $DOTNET_HOME --version 1.1.0 --channel release/1.1.0 --shared-runtime
+fi
 
-mkdir -p artifacts
-pushd artifacts
-    ../src/native/build.sh
-    export PATH=$(pwd)/bin:$PATH
-    dnvm_home=/usr/local/share/dnvm/environments/default
-    if [[ ! -d "$dnvm_home/sdk/1.0.0-preview4-004233" ]] ; then
-        ../scripts/dotnet-install.sh --install-dir $dnvm_home --version 1.0.0-preview4-004233
-    fi
-    if [[ ! -d "$dnvm_home/shared/Microsoft.NETCore.App/1.1.0" ]] ; then
-        ../scripts/dotnet-install.sh --install-dir $dnvm_home --version 1.1.0 --channel release/1.1.0 --shared-runtime
-    fi
-    dotnet restore ../dnvm.sln
-    rm bin/dnvm || :
-    rm -r pub/ || :
-    dotnet publish ../src/dnvm/dnvm.csproj -o $(pwd)/pub -r osx.10.10-x64 /nologo
-    ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib ./pub/
-    ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib ./pub/
-    pushd bin
-    ln -s ../pub/dnvm ./dnvm
-    popd
-    echo "App size = $(du -hd 1 pub/ | awk '{print $1}')"
-popd
+$DOTNET_HOME/dotnet restore /nologo dnvm.sln
+$DOTNET_HOME/dotnet publish /nologo /m build.proj
 
 echo 'Build succeeded'
---
+++ packaging/README.md
@@ -0,0 +1,15 @@
+
+```
+   ___  _  ___   ____  ___
+  / _ \/ |/ / | / /  |/  /
+ / // /    /| |/ / /|_/ /
+/____/_/|_/ |___/_/  /_/
+
+```
+
+dnvm
+----
+
+## How to install
+
+Symlink bin/dotnet and bin/dnvm into your PATH
