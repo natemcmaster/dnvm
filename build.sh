c6c743566c2dad45bf54111bafe3bf0a035f15f6
Nate McMaster <nate.mcmaster@gmail.com>
2017-01-30 22:42:00 -0800

    Upgrade to build with RC3

--- build.sh
+++ build.sh
@@ -3,18 +3,20 @@
 set -e
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-dotnet_version=$(<./toolsversion.txt)
-DOTNET_HOME=/usr/local/share/dnvm/environments/default
-if [[ ! -d "$DOTNET_HOME/sdk/$dotnet_version" ]] ; then
-    ../scripts/dotnet-install.sh --install-dir $DOTNET_HOME --version $dotnet_version
+if [[ "$(uname)" == "Darwin" ]]; then
+    if brew ls --versions dnvm >/dev/null ; then
+        brew outdated && brew install dnvm --without-sdk
+    else
+        brew tap natemcmaster/dnvm
+        brew install dnvm --without-sdk
+    fi
 fi
-if [[ ! -d "$DOTNET_HOME/shared/Microsoft.NETCore.App/1.1.0" ]] ; then
-    ../scripts/dotnet-install.sh --install-dir $DOTNET_HOME --version 1.1.0 --channel release/1.1.0 --shared-runtime
-fi
+
+dnvm install
 
 mkdir -p artifacts/log/
 
-$DOTNET_HOME/dotnet restore /nologo dnvm.sln
-$DOTNET_HOME/dotnet publish build.proj /nologo /m /fl /flp:LogFile=artifacts/log/msbuild.log "$@"
+dotnet restore /nologo dnvm.sln
+dotnet publish build.proj /nologo /m /fl /flp:LogFile=artifacts/log/msbuild.log "$@"
 
 echo 'Build succeeded'
