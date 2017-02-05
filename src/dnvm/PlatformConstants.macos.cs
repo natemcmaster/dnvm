78cd83288a27cc2ce46e7edca47c4489e4acbb69
Nate McMaster <nate.mcmaster@gmail.com>
2017-02-05 09:36:32 -0800

    Add platform constants

---
+++ src/dnvm/PlatformConstants.macos.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet
+{
+    public static class PlatformConstants
+    {
+        public const string RuntimeOSName = "osx";
+        public const string RuntimeOSWithVersion = "osx.10.10";
+        public const string RuntimeOSArch = "x64";
+        public const string RuntimeIdentifer = RuntimeOSWithVersion + "-" + RuntimeOSArch;
+    }
+}
---
+++ src/dnvm/PlatformConstants.win.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet
+{
+    public static class PlatformConstants
+    {
+        public const string RuntimeOSName = "win";
+        public const string RuntimeOSArch = "x64";
+    }
+}
