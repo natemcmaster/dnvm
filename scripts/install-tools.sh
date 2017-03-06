7bb0f7d8e9bb9cc2cbfcbbb4b4b1a810b7aa2d4c
Nate McMaster <nate.mcmaster@gmail.com>
2017-03-05 21:45:19 -0800

    Update travis settings

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -4,13 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/common.sh
 
 if [ "$(uname)" = "Darwin" ]; then
-    if ! [ "$TRAVIS" = true ]; then
-        brew outdated cmake || __exec brew install cmake
-    fi
+    __exec brew install cmake
+    brew outdated cmake || __exec brew install cmake
     __exec brew install libyaml
     brew outdated libyaml || __exec brew install libyaml
     __exec brew tap natemcmaster/dnvm
-    __exec brew install dnvm
+    __exec brew install dnvm --without-sdk
     brew outdated dnvm || __exec brew install dnvm
 fi
 
