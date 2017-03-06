ec11ce298d1ed52c4150d14b91c359d0c73b85ce
Nate McMaster <nate.mcmaster@gmail.com>
2017-03-05 21:49:29 -0800

    Grr, travis, plz build

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -4,10 +4,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/common.sh
 
 if [ "$(uname)" = "Darwin" ]; then
-    __exec brew install cmake
-    brew outdated cmake || __exec brew install cmake
+    if ! [ "$TRAVIS" = true ]; then
+        __exec brew install cmake
+        brew outdated cmake || __exec brew install cmake
+    fi
+
     __exec brew install libyaml
     brew outdated libyaml || __exec brew install libyaml
+
     __exec brew tap natemcmaster/dnvm
     __exec brew install dnvm --without-sdk
     brew outdated dnvm || __exec brew install dnvm
